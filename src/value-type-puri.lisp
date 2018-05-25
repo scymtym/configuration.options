8c08ef0cc9830f6d95dcd28b0884485c38802eed
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-05-25 10:05:04 +0200

    Added proper-puri value type in src/value-type-puri.lisp
    A "proper" URI is an URI with a non-empty scheme, i.e. one that does
    not look like a typical filesystem path.
    
    * src/value-type-puri.lisp (header): updated copyright
      (proper-puri): new type; an URI with a non-empty scheme
      (validate-value-using-type type-based-validation-mixin t eql proper-puri):
      new method; validation for type proper-puri
      (value->string-using-type type-based-conversion-mixin uri eql proper-puri):
      new method; unparsing for type proper-puri
      (raw->value-using-type type-based-conversion-mixin string eql proper-puri):
      new method; parsing for type proper-puri
    * test/value-type-puri.lisp (header): updated copyright
      (package configuration.options.puri.test): added imported-from
      configuration.options symbol proper-puri
      (test validate-value): added cases for `proper-puri' type; adapted
      documentation string accordingly
      (test value<->string): similar

--- src/value-type-puri.lisp
+++ src/value-type-puri.lisp
@@ -1,10 +1,12 @@
 ;;;; value-type-puri.lisp --- Conversion between strings and parsed URIs.
 ;;;;
-;;;; Copyright (C) 2013, 2014, 2016, 2017 Jan Moringen
+;;;; Copyright (C) 2013-2018 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:configuration.options)
+
+;;; `puri:uri' value type
 
 (defmethod value->string-using-type ((schema-item type-based-conversion-mixin)
                                      (value       puri:uri)
@@ -18,3 +20,33 @@
                                   (type        (eql 'puri:uri))
                                   &key &allow-other-keys)
   (puri:uri value))
+
+;;; `proper-puri' value type
+
+(defun proper-puri-p (thing)
+  (and (typep thing 'puri:uri) (puri:uri-scheme thing)))
+
+(deftype proper-puri ()
+  '(and puri:uri (satisfies proper-puri-p)))
+
+(setf (get 'proper-puri 'dont-expand) t)
+
+(defmethod value->string-using-type ((schema-item type-based-conversion-mixin)
+                                     (value       puri:uri)
+                                     (type        (eql 'proper-puri))
+                                     &rest args &key &allow-other-keys)
+  (unless (proper-puri-p value)
+    (error "~@<~S is not a \"proper\" URI because it does not have a ~
+           scheme component.~@:>"
+           value))
+  (apply #'value->string-using-type schema-item value 'puri:uri args))
+
+(defmethod raw->value-using-type ((schema-item type-based-conversion-mixin)
+                                  (value       string)
+                                  (type        (eql 'proper-puri))
+                                  &rest args &key &allow-other-keys)
+  (if-let ((colon (position #\: value)))
+    (apply #'raw->value-using-type schema-item value 'puri:uri args)
+    (error "~@<\"~A\" is not a \"proper\" URI because it does not have ~
+            a scheme component.~@:>"
+           value)))
--- test/value-type-puri.lisp
+++ test/value-type-puri.lisp
@@ -1,6 +1,6 @@
 ;;;; value-type-puri.lisp --- Unit tests for puri instances as values.
 ;;;;
-;;;; Copyright (C) 2013, 2016, 2017 Jan Moringen
+;;;; Copyright (C) 2013-2018 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -13,6 +13,9 @@
    #:configuration.options
 
    #:fiveam)
+
+  (:import-from #:configuration.options
+   #:proper-puri)
 
   (:import-from #:configuration.options.test
    #:check-validate-value
@@ -32,19 +35,30 @@
 (test validate-value
   "Test for methods on `validate-value' for `puri:uri' objects."
 
-  (mapc (lambda+ ((value expected))
-          (check-validate-value value 'puri:uri expected))
-        `((nil                 nil)
-          (t                   nil)
-          (1                   nil)
-          (,(puri:uri "http:") t))))
+  (mapc (lambda+ ((type value expected))
+          (check-validate-value value type expected))
+        `((puri:uri    nil                     nil)
+          (puri:uri    t                       nil)
+          (puri:uri    1                       nil)
+          (puri:uri    ,(puri:uri "http:")     t)
+
+          (proper-puri ,(puri:uri "foo")       nil)
+          (proper-puri ,(puri:uri "/foo/bar")  nil)
+          (proper-puri ,(puri:uri "http:")     t)
+          (proper-puri ,(puri:uri "http:/foo") t))))
 
 (test value<->string
-  "Test `value->string' and `raw->value' methods for `puri:uri' type."
+  "Test `value->string' and `raw->value' methods for `puri:uri' and
+   `proper-puri' types."
 
-  (mapc (lambda+ ((string value))
-          (check-value<->string 'puri:uri string value :value-test #'puri:uri=))
-        `(("1 2"    option-syntax-error)
-          ("//"     option-syntax-error)
-          (":"      option-syntax-error)
-          ("http:/" ,(puri:uri "http:")))))
+  (mapc (lambda+ ((type string value))
+          (check-value<->string type string value :value-test #'puri:uri=))
+        `((puri:uri    "1 2"       option-syntax-error)
+          (puri:uri    "//"        option-syntax-error)
+          (puri:uri    ":"         option-syntax-error)
+          (puri:uri    "http:/"    ,(puri:uri "http:"))
+
+          (proper-puri "foo"       option-syntax-error)
+          (proper-puri "/foo/bar"  option-syntax-error)
+          (proper-puri "http:/"    ,(puri:uri "http:"))
+          (proper-puri "http:/foo" ,(puri:uri "http:/foo")))))
